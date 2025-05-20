ef0d9bedb44728bbdf76acad2deafe5662990158
mikel-elorrieta <mikel.martinca@elorrieta-errekamari.com>
2025-05-20 22:23:53 +0200

    amaituta v2

---
+++ TFG/IlaraBegiratu/app/src/main/java/com/example/ilaraBegiratu/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.ilaraBegiratu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DBHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(com.example.ilaraBegiratu.R.layout.activity_main);
+
+        dbHelper = new DBHelper(this);
+
+        dbHelper.getWritableDatabase();
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(this, Login.class));
+            finish();
+        }, 2000);
+    }
+}
